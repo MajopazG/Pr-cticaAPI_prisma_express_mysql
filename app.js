cd4ca0a846814e65bd160e97b907fb854afc42ff
Maria Jose Paz Garcia <majopazgarcia25@gmail.com>
2023-02-17 12:04:02 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const {PrismaClient} = require('@prisma/client')
+
+const prisma = new PrismaClient()
+const app = express ()
+
+app.use(express.json())
+
+app.get('/', (req, res)=> {
+    res.send('Hola mundo')
+}) 
+// '/'=>ruta raiz
+
+//Crear un registro
+app.post(`/post`, async(req, res) =>{
+    const {title, content} = req.body
+    const result = await prisma.post.create({
+        data: {
+            title, content
+        }
+    })
+    res.json(result)
+})
+
+//Mostrar todos los registros 
+app.get(`/post`, async(req, res) =>{
+    const posts = await prisma.post.findMany()
+    res.json(posts)
+})
+
+//Actualizar un registro
+app.put(`/post/:id`, async(req, res) =>{
+    const {id} = req.params
+    const {title, content} = req.body
+    const post = await prisma.post.update({
+        where: {id: Number(id)},
+        data: {title, content}
+    })
+    res.json(post)
+})
+
+//Eliminar un registro
+app.delete(`/post/:id`, async(req, res)=>{
+    const {id} = req.params
+    const post = await prisma.post.delete({
+        where: {id: Number(id)}
+    })
+    res.json('Eliminado')
+})
+
+app.listen(3000, () =>
+    console.log(`Server ready at: htt://localhost:3000`)
+)
